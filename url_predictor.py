2397d6a19abc0d48609ed916b766229c5548dc38
tfiers <tomas.fiers@gmail.com>
2016-05-05 18:12:19 +0200

    Constructed pages graph; Searched for paths.

--- url_predictor.py
+++ url_predictor.py
@@ -3,6 +3,9 @@
 import json
 from dateutil.parser import parse as parse_datetime
 
+# Keys are urls.
+# Values are lists [[url, score], [url, score], ..]
+# of all urls that can be reached from the current page.
 model = {}
 
 
@@ -15,16 +18,7 @@ def get_guesses(url):
     [['https://dtai.cs.kuleuven.be/events/leuveninc', 0.9],
      ['https://google.com', 0.5]]
     """
-    if url not in model:
-        return []
-    else:
-        results = []
-        target_url_counts = model[url]
-        for url, count in target_url_counts.items():
-            results.append([url, count])
-        # Sort from highest to lowest count.
-        results.sort(key=lambda e: e[1], reverse=True)
-        return results
+    return model.get(url, [])
 
 
 def handle_csv(csv_file_handle):
@@ -50,7 +44,7 @@ def parse(lines):
         # returned.
         events.append({
             't': parse_datetime(data[0]),
-            'event_type': data[1], 
+            'event_type': data[1],
             'url': data[2],
             'target': data[3],
         })
@@ -59,7 +53,7 @@ def parse(lines):
 
 def make_page_visits(events):
     """ Constructs page visit objects (as dictionaries).
-    See the "Data" section in the report and the accompanying figure.
+    See the "Data" section in the report and its accompanying figure.
     """
     # The list we'll fill with page visit objects.
     page_visits = []
@@ -90,19 +84,100 @@ def make_page_visits(events):
     return page_visits
 
 
+def make_graph(page_visits):
+    """ Constructs a directed graph from the page visits list.
+    """
+    # The graph is a dictionary of nodes. The keys are page urls,
+    # the values contain urls of pages following the page of the key 
+    # url, and the number of times the key url page was visited.
+    nodes = {}
+    for i in range(len(page_visits)):
+        # Get the url of the currently visited page.
+        url = page_visits[i]['url']
+        # If the visited page is already a node in the graph..
+        if url in nodes:
+            # .. increase the number of times it was visited.
+            nodes[url]['num_visits'] += 1
+        else:
+            # If not, add a new node, with currently one visit.
+            nodes[url] = {
+                'num_visits': 1,
+                'linked_urls': {},
+            }
+        # Get a reference to the urls linked from the current node.
+        linked_urls = nodes[url]['linked_urls']
+        # If this is not the last page visit of the browsing session..
+        if i < len(page_visits)-1: 
+            # .. get the url of the page visited next.
+            next_url = page_visits[i+1]['url']
+            # If there is already a link from the current page to the 
+            # other url..
+            if next_url in linked_urls:
+                # .. increase the number of times the page at 
+                # 'next_url' followed the current page.
+                linked_urls[next_url] += 1
+            else:
+                # If not, add a new link. (Currently followed once).
+                linked_urls[next_url] = 1
+    # Return the graph.
+    return nodes
+
+
 def preprocess(csv_file_handle):
     """ 
     """
     # Read the csv file as a list of strings.
     lines = csv_file.readlines()
     # Make a list of 'event' dictionaries.
-    events = parse(csv_file_handle)
+    events = parse(lines)
     # Make a list of 'page visit' dictionaries.
     page_visits = make_page_visits(events)
+    # Make a graph (as a dictionary of nodes), discarding absolute
+    # temporal information and higher-order sequential information.
+    nodes = make_graph(page_visits)
 
 
-def learn(entries):
-    """ Trains a model that predicts the most likely destination page
+def learn(nodes):
+    """ Trains a model that predicts the most likely destination pages
     for each starting url.
     """
     pass
+    # For each starting url:
+    #   - Traverse graph recursively.
+    #   - 
+
+
+def find_paths(nodes, current_url, end_url, travel_history=()):
+    """ Recursively calculates all paths without loops through the 
+    graph 'nodes' from 'current_url' to 'end_url'.
+    'travel_history' is the sequence of urls already "visited" in the
+    current traversal.
+    """
+    # Extend our travel history with the page we're currently at.
+    # We work with tuples because they are immutable.
+    travel_history += (current_url,)
+    # If we're at the end of the recursion..
+    if current_url == end_url:
+        # .. return a list of only one path.
+        return [travel_history]
+
+    # The list of paths from the current node (url / page) to the
+    # destination url that we'll return.
+    paths = []
+    # A dictionary of pages directly reachable from the current page.
+    linked_urls = nodes[current_url]['linked_urls']
+    # print(linked_urls)
+    # For each such page..
+    for next_url in linked_urls:
+        # .. if it is not already in the travel history 
+        # (to avoid loops)..
+        if next_url not in travel_history:
+            # .. ask for a list of paths from it to the destination 
+            # page.
+            new_paths = find_paths(nodes, next_url, end_url, travel_history)
+            # Add each such new (complete) path to the list of paths
+            # we'll return.
+            paths += new_paths
+    # Return the list of paths.
+    return paths
+

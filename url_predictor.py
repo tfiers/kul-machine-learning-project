9196279da684dff128cc30d4a85f3e229b1371ba
tfiers <tomas.fiers@gmail.com>
2016-05-07 13:44:49 +0200

    Full model. (Needs some rework for efficiency).

--- url_predictor.py
+++ url_predictor.py
@@ -14,22 +14,12 @@ def get_domain(url):
     return psl.get_public_suffix(netloc)
 
 
-# Keys are urls.
-# Values are lists [[url, score], [url, score], ..]
-# of all urls that can be reached from the current page.
-model = {}
+# # Keys are urls.
+# # Values are lists [[url, info], [url, info], ..]
+# # of all urls that can be reached from the current page.
+# model = {}
 
-
-def get_guesses(url):
-    """ Given a url, returns a list of the most probable destination
-    urls. Each entry is of the form [url, probability_score].
-    The entries are sorted from highest to lowest probabibility.
-
-    Example:
-    [['https://dtai.cs.kuleuven.be/events/leuveninc', 0.9],
-     ['https://google.com', 0.5]]
-    """
-    return model.get(url, [])
+feature_matrix = {}
 
 
 def handle_csv(csv_file_handle):
@@ -37,8 +27,33 @@ def handle_csv(csv_file_handle):
     (The csv file should be supplied as a file handle.
     The model is stored in the global variable 'model').
     """
-    preprocessed_data = preprocess(csv_file_handle)
-    learn(preprocessed_data)
+    learn(csv_file_handle)
+
+
+def learn(csv_file_handle):
+    """ Trains a model that predicts the most likely destination pages
+    for each starting url.
+    """
+    # PREPROCESSING
+    # 
+    # Read the csv file as a list of strings.
+    lines = csv_file_handle.readlines()
+    # Make a list of 'event' dictionaries.
+    events = parse(lines)
+    # Make a list of 'page visit' dictionaries.
+    page_visits = make_page_visits(events)
+    # Make a graph (as a dictionary of nodes), discarding absolute
+    # temporal information and higher-order sequential information.
+    nodes = make_graph(page_visits)
+    # Annotate each node with the average time spent on that page.
+    # ToDo: f = median, average of max ?
+    for P in nodes.values():
+        durations = [d.total_seconds() for d in P['durations']]
+        P['avg_duration'] = average(durations)
+
+    # TRAINING MODEL
+    # 
+    feature_matrix = calculate_features(nodes)
 
 
 def parse(lines):
@@ -77,7 +92,7 @@ def make_page_visits(events):
             next_event = events[i+1]
             # Determine the amount of time the user stayed on this 
             # page. The result is a 'timedelta' object.
-            dt = next_event['t'] - curent_event['t']
+            duration = next_event['t'] - curent_event['t']
             # Determine how this page was left.
             if next_event['event_type'] == 'click':
                 exit_type = 'click'
@@ -88,8 +103,8 @@ def make_page_visits(events):
             # Create a new page visit object.
             page_visits.append({
                 'url': curent_event['url'],
-                't': curent_event['t'], # = time of event
-                'dt' : dt, # dt = delta-t = duration of stay
+                't': curent_event['t'], # = time of page entry
+                'duration' : duration, # = duration of stay
                 'exit_type': exit_type,
             })
     return page_visits
@@ -105,19 +120,19 @@ def make_graph(page_visits):
     for i in range(len(page_visits)):
         # Get the url of the currently visited page.
         url = page_visits[i]['url']
-        dt  = page_visits[i]['dt']
+        duration  = page_visits[i]['duration']
         # If the visited page is already a node in the graph..
         if url in nodes:
             # .. increase the number of times it was visited.
             nodes[url]['num_visits'] += 1
             # And add how long the user stayed on this page during 
             # this page visit.
-            nodes[url]['dts'] += [dt]
+            nodes[url]['durations'] += [duration]
         else:
             # If not, add a new node, with currently one visit.
             nodes[url] = {
                 'num_visits': 1,
-                'dts': [dt],
+                'durations': [duration],
                 'domain': get_domain(url),
                 'linked_urls': {},
             }
@@ -140,39 +155,47 @@ def make_graph(page_visits):
     return nodes
 
 
-def preprocess(csv_file_handle):
+def average(iterable):
+    """ The input needs to have a finite length.
+    """
+    return sum(iterable) / len(iterable) # Py3
+
+
+# For each starting url:
+#     For each other url = destination url:
+#         - Find all paths
+#         - Calculate probabilities of each path
+#         - Sum weighted probabilities (higher weight to longer paths).
+#         - Add term for same domain.
+#         - Add term for [average] time spent on page.
+
+def calculate_features(nodes):
     """ 
     """
-    # Read the csv file as a list of strings.
-    lines = csv_file.readlines()
-    # Make a list of 'event' dictionaries.
-    events = parse(lines)
-    # Make a list of 'page visit' dictionaries.
-    page_visits = make_page_visits(events)
-    # Make a graph (as a dictionary of nodes), discarding absolute
-    # temporal information and higher-order sequential information.
-    nodes = make_graph(page_visits)
-
-
-def learn(nodes):
-    """ Trains a model that predicts the most likely destination pages
-    for each starting url.
-    """
-    pass
-    # For each starting url:
-    #     For each other url = destination url:
-    #         - Find all paths
-    #         - Calculate probabilities of each path
-    #         - Sum weighted probabilities (higher weight to longer paths).
-    #         - Add term for same domain.
-    #         - Add term for [average] time spent on page.
+    feature_matrix = {}
+    total_page_visits = sum(P['num_visits'] for P in nodes.values())
+    avg_avg_duration = average([P['avg_duration'] for P in nodes.values()])
+    for url_1, P1 in nodes.items():
+        print('url_1: {}'.format(url_1))
+        feature_matrix[url_1] = {}
+        for url_2, P2 in nodes.items():
+            if url_1 == url_2:
+                continue
+            print('\turl_2: {}'.format(url_2))
+            feature_matrix[url_1][url_2] = {
+                'paths': find_paths(nodes, url_1, url_2),
+                'same_domain': (P1['domain'] == P2['domain']),
+                'absolute_p': P2['num_visits'] / total_page_visits, # Py3
+                'relative_duration': P2['avg_duration'] / avg_avg_duration,
+            }
+    return feature_matrix
 
 
 def find_paths(nodes, current_url, end_url, travel_history=()):
-    """ Recursively calculates all paths without loops through the 
-    graph defined by 'nodes' from 'current_url' to 'end_url'.
-    'travel_history' is the sequence of urls already "visited" in the
-    current traversal.
+    """ Recursively calculates all paths without loops from 
+    'current_url' to 'end_url' through the graph defined by 'nodes'.
+    ('travel_history' is the sequence of urls already "visited" in the
+    current traversal. Only used internally.)
     """
     # Extend our travel history with the page we're currently at.
     # We work with tuples because they are immutable. 
@@ -204,6 +227,12 @@ def find_paths(nodes, current_url, end_url, travel_history=()):
     return paths
 
 
+
+
+# --------------------------------------------------------------------
+
+
+
 def probability(nodes, path):
     """ Calculates the probability that a random walker takes
     the given path trough the graph defined by 'nodes'.
@@ -214,7 +243,60 @@ def probability(nodes, path):
         next_url = path[i+1]
         total_visits = nodes[current_url]['num_visits']
         visits_to_next = nodes[current_url]['linked_urls'][next_url]
-        transition_p = visits_to_next / total_visits # Watch out in Py2
+        transition_p = visits_to_next / total_visits # Py3
+        # print('{:.6f}  {}/{}  {}'.format(p, visits_to_next, total_visits, current_url))
         p *= transition_p
-        print('{:.6f}  {:.6f}  {}'.format(p, transition_p, next_url))
+    # print('{:.6f}       {}'.format(p, next_url))
     return p
+
+def get_guesses(url):
+    """ Given a url, returns a list of the most probable destination
+    urls. Each entry is of the form [url, info].
+    The entries are sorted from highest to lowest probabibility.
+
+    Example:
+    [['https://dtai.cs.kuleuven.be/events/leuveninc', 0.9],
+     ['https://google.com', 0.5]]
+    """
+    url_1 = url
+    destinations = []
+    for url_2 in feature_matrix[url]:
+        score = calculate_score(url_1, url_2, feature_matrix)
+        destinations.append([
+            url_2,
+            format_destination(url_2, score),
+            score,
+        ])
+    destinations = sorted(destinations,
+                          key=lambda d: d[-1],
+                          reverse=True)
+    return destinations[:3]
+
+
+def calculate_score(url_1, url_2, feature_matrix):
+    """ 
+    """
+    f = feature_matrix[url_1][url_2]
+    p_abs = f['absolute_p']
+    p_rel = sum(probability(p) * (1.1)**len(p) for p in path)
+    T = f['relative_duration']
+    D = f['same_domain'] + 0.5
+    # P(P2|P1) =  P(P2)        *    P(P1|P2)
+    return        p_abs * T    *    p_rel * D
+
+
+def format_destination(url, score):
+    """ 
+    """
+    return '{}  {}'.format(shorten_url(url), score)
+
+
+def shorten_url(url):
+    """ 
+    """
+    MAX_LEN = 50
+    if len(url) <= MAX_LEN:
+        return url
+    else:
+        n = MAX_LEN//2
+        return '{}...{}'.format(url[:n-1], url[-n+2:])
